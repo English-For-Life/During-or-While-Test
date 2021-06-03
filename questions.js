622ca98ce29ba8ed00a8f406390972badd03db5a
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-06-03 18:24:25 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"I met Mike ..... I was shopping.",
+        options:["while", "during"],
+        answer: 0
+    },
+    {
+        q:" ..... I was on holiday, I didn’t use my phone at all.",
+        options:["During", "While"],
+        answer: 1
+    },
+    {
+        q:"I learnt a lot ..... the course. The teachers were very good.",
+        options:["during", "while"],
+        answer: 0
+    },
+    {
+        q:"We met a lot of interesting people ..... our holiday.",
+        options:["while", "during" ],
+        answer: 1
+    },
+    {
+        q:"We met a lot of interesting people ..... we were on holiday.",
+        options:["while", "during"],
+        answer: 0
+    },
+    {
+        q:"What did they say about me ..... I was out of the room?",
+        options:["during", "while"],
+        answer: 1
+    },
+    {
+     q:"When I fly anywhere, I never eat anything ..... the flight.",
+        options:["during", "while"],
+        answer: 0   
+    },
+    {
+        q:"Please don’t interrupt me ..... I’m speaking.",
+        options:["during", "while"],
+        answer: 1
+    },
+    {
+        q:" ..... the festival, it’s almost impossible to find a hotel room here.",
+        options:["During", "While"],
+        answer: 0
+    },
+    {
+        q:"We were hungry when we arrived. We hadn’t eaten anything ..... we were travelling.",
+        options:["during", "while"],
+        answer: 1
+    }
+
+]
